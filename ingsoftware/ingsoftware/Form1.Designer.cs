c055685dab5ee2aae89de1913ae6c131cf0ac184
jazminpimentel <jazmin_pimentel@hotmail.es>
2016-12-13 12:42:43 -0700

    AGREGARON NOMBRES STANDAR

--- ingsoftware/ingsoftware/Form1.Designer.cs
+++ ingsoftware/ingsoftware/Form1.Designer.cs
@@ -37,12 +37,12 @@
             this.obrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nuevaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cotizacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.gastoPorObraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.totalSemanalPorObraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.horasExtrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.totalDePagoPorEmpleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.cotizacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,7 +53,8 @@
             this.reportesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(840, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(630, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,7 +64,7 @@
             this.trabajadoresToolStripMenuItem,
             this.obrasToolStripMenuItem});
             this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            this.menuToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.menuToolStripMenuItem.Text = "Menu";
             this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
             // 
@@ -74,26 +75,26 @@
             this.editarToolStripMenuItem,
             this.eliminarToolStripMenuItem});
             this.trabajadoresToolStripMenuItem.Name = "trabajadoresToolStripMenuItem";
-            this.trabajadoresToolStripMenuItem.Size = new System.Drawing.Size(164, 24);
+            this.trabajadoresToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
             this.trabajadoresToolStripMenuItem.Text = "Trabajadores";
             // 
             // nuevoToolStripMenuItem
             // 
             this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
-            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(132, 24);
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.nuevoToolStripMenuItem.Text = "Nuevo";
             this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
             // 
             // editarToolStripMenuItem
             // 
             this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
-            this.editarToolStripMenuItem.Size = new System.Drawing.Size(132, 24);
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.editarToolStripMenuItem.Text = "Editar";
             // 
             // eliminarToolStripMenuItem
             // 
             this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
-            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(132, 24);
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.eliminarToolStripMenuItem.Text = "Eliminar";
             this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click);
             // 
@@ -104,20 +105,26 @@
             this.editarToolStripMenuItem1,
             this.cotizacionToolStripMenuItem});
             this.obrasToolStripMenuItem.Name = "obrasToolStripMenuItem";
-            this.obrasToolStripMenuItem.Size = new System.Drawing.Size(164, 24);
+            this.obrasToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
             this.obrasToolStripMenuItem.Text = "Obras";
             // 
             // nuevaToolStripMenuItem
             // 
             this.nuevaToolStripMenuItem.Name = "nuevaToolStripMenuItem";
-            this.nuevaToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.nuevaToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.nuevaToolStripMenuItem.Text = "Nueva";
             // 
             // editarToolStripMenuItem1
             // 
             this.editarToolStripMenuItem1.Name = "editarToolStripMenuItem1";
-            this.editarToolStripMenuItem1.Size = new System.Drawing.Size(152, 24);
+            this.editarToolStripMenuItem1.Size = new System.Drawing.Size(130, 22);
             this.editarToolStripMenuItem1.Text = "Editar";
+            // 
+            // cotizacionToolStripMenuItem
+            // 
+            this.cotizacionToolStripMenuItem.Name = "cotizacionToolStripMenuItem";
+            this.cotizacionToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.cotizacionToolStripMenuItem.Text = "Cotizacion";
             // 
             // reportesToolStripMenuItem
             // 
@@ -127,48 +134,43 @@
             this.horasExtrasToolStripMenuItem,
             this.totalDePagoPorEmpleadoToolStripMenuItem});
             this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
-            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
             this.reportesToolStripMenuItem.Text = "Reportes";
             // 
             // gastoPorObraToolStripMenuItem
             // 
             this.gastoPorObraToolStripMenuItem.Name = "gastoPorObraToolStripMenuItem";
-            this.gastoPorObraToolStripMenuItem.Size = new System.Drawing.Size(234, 24);
+            this.gastoPorObraToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
             this.gastoPorObraToolStripMenuItem.Text = "Gasto Por Obra";
             // 
             // totalSemanalPorObraToolStripMenuItem
             // 
             this.totalSemanalPorObraToolStripMenuItem.Name = "totalSemanalPorObraToolStripMenuItem";
-            this.totalSemanalPorObraToolStripMenuItem.Size = new System.Drawing.Size(234, 24);
+            this.totalSemanalPorObraToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
             this.totalSemanalPorObraToolStripMenuItem.Text = "Total Semanal Por Obra";
             // 
             // horasExtrasToolStripMenuItem
             // 
             this.horasExtrasToolStripMenuItem.Name = "horasExtrasToolStripMenuItem";
-            this.horasExtrasToolStripMenuItem.Size = new System.Drawing.Size(234, 24);
+            this.horasExtrasToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
             this.horasExtrasToolStripMenuItem.Text = "Horas Extras";
             // 
             // totalDePagoPorEmpleadoToolStripMenuItem
             // 
             this.totalDePagoPorEmpleadoToolStripMenuItem.Name = "totalDePagoPorEmpleadoToolStripMenuItem";
-            this.totalDePagoPorEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(268, 24);
+            this.totalDePagoPorEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
             this.totalDePagoPorEmpleadoToolStripMenuItem.Text = "Total De Pago Por Empleado";
-            // 
-            // cotizacionToolStripMenuItem
-            // 
-            this.cotizacionToolStripMenuItem.Name = "cotizacionToolStripMenuItem";
-            this.cotizacionToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
-            this.cotizacionToolStripMenuItem.Text = "Cotizacion";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 408);
+            this.ClientSize = new System.Drawing.Size(630, 332);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Jguz Registro";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- ingsoftware/ingsoftware/obra.Designer.cs
+++ ingsoftware/ingsoftware/obra.Designer.cs
@@ -31,21 +31,23 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_num_contrato = new System.Windows.Forms.TextBox();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
-            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.cb_estado = new System.Windows.Forms.ComboBox();
+            this.txt_cliente = new System.Windows.Forms.TextBox();
+            this.cb_dia_ini = new System.Windows.Forms.ComboBox();
+            this.cb_mes_ini = new System.Windows.Forms.ComboBox();
+            this.cb_año_ini = new System.Windows.Forms.ComboBox();
+            this.cb_dia_fin = new System.Windows.Forms.ComboBox();
+            this.cb_mes_fin = new System.Windows.Forms.ComboBox();
+            this.cb_año_fin = new System.Windows.Forms.ComboBox();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -79,29 +81,29 @@
             this.label3.Text = "Direccion ";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // textBox1
+            // txt_nombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(123, 56);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(144, 20);
-            this.textBox1.TabIndex = 3;
+            this.txt_nombre.Location = new System.Drawing.Point(123, 56);
+            this.txt_nombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(144, 20);
+            this.txt_nombre.TabIndex = 3;
             // 
-            // textBox2
+            // txt_num_contrato
             // 
-            this.textBox2.Location = new System.Drawing.Point(123, 92);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(144, 20);
-            this.textBox2.TabIndex = 4;
+            this.txt_num_contrato.Location = new System.Drawing.Point(123, 92);
+            this.txt_num_contrato.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_num_contrato.Name = "txt_num_contrato";
+            this.txt_num_contrato.Size = new System.Drawing.Size(144, 20);
+            this.txt_num_contrato.TabIndex = 4;
             // 
-            // textBox3
+            // txt_direccion
             // 
-            this.textBox3.Location = new System.Drawing.Point(123, 125);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(144, 20);
-            this.textBox3.TabIndex = 5;
+            this.txt_direccion.Location = new System.Drawing.Point(123, 125);
+            this.txt_direccion.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(144, 20);
+            this.txt_direccion.TabIndex = 5;
             // 
             // label4
             // 
@@ -143,101 +145,121 @@
             this.label7.TabIndex = 9;
             this.label7.Text = "Cliente";
             // 
-            // comboBox1
+            // cb_estado
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(62, 264);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(62, 21);
-            this.comboBox1.TabIndex = 10;
+            this.cb_estado.FormattingEnabled = true;
+            this.cb_estado.Location = new System.Drawing.Point(62, 264);
+            this.cb_estado.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_estado.Name = "cb_estado";
+            this.cb_estado.Size = new System.Drawing.Size(62, 21);
+            this.cb_estado.TabIndex = 10;
             // 
-            // textBox4
+            // txt_cliente
             // 
-            this.textBox4.Location = new System.Drawing.Point(174, 266);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(84, 20);
-            this.textBox4.TabIndex = 11;
+            this.txt_cliente.Location = new System.Drawing.Point(174, 266);
+            this.txt_cliente.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_cliente.Name = "txt_cliente";
+            this.txt_cliente.Size = new System.Drawing.Size(84, 20);
+            this.txt_cliente.TabIndex = 11;
             // 
-            // comboBox2
+            // cb_dia_ini
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(88, 177);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(42, 21);
-            this.comboBox2.TabIndex = 12;
+            this.cb_dia_ini.FormattingEnabled = true;
+            this.cb_dia_ini.Location = new System.Drawing.Point(88, 177);
+            this.cb_dia_ini.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_dia_ini.Name = "cb_dia_ini";
+            this.cb_dia_ini.Size = new System.Drawing.Size(42, 21);
+            this.cb_dia_ini.TabIndex = 12;
             // 
-            // comboBox3
+            // cb_mes_ini
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(134, 177);
-            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(41, 21);
-            this.comboBox3.TabIndex = 13;
+            this.cb_mes_ini.FormattingEnabled = true;
+            this.cb_mes_ini.Location = new System.Drawing.Point(134, 177);
+            this.cb_mes_ini.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_mes_ini.Name = "cb_mes_ini";
+            this.cb_mes_ini.Size = new System.Drawing.Size(41, 21);
+            this.cb_mes_ini.TabIndex = 13;
             // 
-            // comboBox4
+            // cb_año_ini
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(182, 177);
-            this.comboBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(92, 21);
-            this.comboBox4.TabIndex = 14;
+            this.cb_año_ini.FormattingEnabled = true;
+            this.cb_año_ini.Location = new System.Drawing.Point(182, 177);
+            this.cb_año_ini.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_año_ini.Name = "cb_año_ini";
+            this.cb_año_ini.Size = new System.Drawing.Size(92, 21);
+            this.cb_año_ini.TabIndex = 14;
             // 
-            // comboBox5
+            // cb_dia_fin
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(88, 215);
-            this.comboBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(42, 21);
-            this.comboBox5.TabIndex = 15;
+            this.cb_dia_fin.FormattingEnabled = true;
+            this.cb_dia_fin.Location = new System.Drawing.Point(88, 215);
+            this.cb_dia_fin.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_dia_fin.Name = "cb_dia_fin";
+            this.cb_dia_fin.Size = new System.Drawing.Size(42, 21);
+            this.cb_dia_fin.TabIndex = 15;
             // 
-            // comboBox6
+            // cb_mes_fin
             // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(134, 215);
-            this.comboBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(41, 21);
-            this.comboBox6.TabIndex = 16;
+            this.cb_mes_fin.FormattingEnabled = true;
+            this.cb_mes_fin.Location = new System.Drawing.Point(134, 215);
+            this.cb_mes_fin.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_mes_fin.Name = "cb_mes_fin";
+            this.cb_mes_fin.Size = new System.Drawing.Size(41, 21);
+            this.cb_mes_fin.TabIndex = 16;
             // 
-            // comboBox7
+            // cb_año_fin
             // 
-            this.comboBox7.FormattingEnabled = true;
-            this.comboBox7.Location = new System.Drawing.Point(182, 215);
-            this.comboBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBox7.Name = "comboBox7";
-            this.comboBox7.Size = new System.Drawing.Size(92, 21);
-            this.comboBox7.TabIndex = 17;
+            this.cb_año_fin.FormattingEnabled = true;
+            this.cb_año_fin.Location = new System.Drawing.Point(182, 215);
+            this.cb_año_fin.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_año_fin.Name = "cb_año_fin";
+            this.cb_año_fin.Size = new System.Drawing.Size(92, 21);
+            this.cb_año_fin.TabIndex = 17;
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Location = new System.Drawing.Point(123, 328);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(75, 23);
+            this.btn_guardar.TabIndex = 18;
+            this.btn_guardar.Text = "GUARDAR";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(214, 328);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancelar.TabIndex = 19;
+            this.btn_cancelar.Text = "CANCELAR";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
             // 
             // obra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 313);
-            this.Controls.Add(this.comboBox7);
-            this.Controls.Add(this.comboBox6);
-            this.Controls.Add(this.comboBox5);
-            this.Controls.Add(this.comboBox4);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(311, 377);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.cb_año_fin);
+            this.Controls.Add(this.cb_mes_fin);
+            this.Controls.Add(this.cb_dia_fin);
+            this.Controls.Add(this.cb_año_ini);
+            this.Controls.Add(this.cb_mes_ini);
+            this.Controls.Add(this.cb_dia_ini);
+            this.Controls.Add(this.txt_cliente);
+            this.Controls.Add(this.cb_estado);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_direccion);
+            this.Controls.Add(this.txt_num_contrato);
+            this.Controls.Add(this.txt_nombre);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "obra";
             this.Text = "obra";
             this.ResumeLayout(false);
@@ -250,20 +272,22 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_num_contrato;
+        private System.Windows.Forms.TextBox txt_direccion;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.ComboBox cb_estado;
+        private System.Windows.Forms.TextBox txt_cliente;
+        private System.Windows.Forms.ComboBox cb_dia_ini;
+        private System.Windows.Forms.ComboBox cb_mes_ini;
+        private System.Windows.Forms.ComboBox cb_año_ini;
+        private System.Windows.Forms.ComboBox cb_dia_fin;
+        private System.Windows.Forms.ComboBox cb_mes_fin;
+        private System.Windows.Forms.ComboBox cb_año_fin;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_cancelar;
     }
 }
