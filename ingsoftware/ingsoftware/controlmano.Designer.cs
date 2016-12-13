44636815947b219585f4e9d8cabdc75bc5bd7b66
jazminpimentel <jazmin_pimentel@hotmail.es>
2016-12-13 12:20:48 -0700

    se hizo algo

--- ingsoftware/ingsoftware/controlmano.Designer.cs
+++ ingsoftware/ingsoftware/controlmano.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.Lbl_trabajador = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -37,75 +37,85 @@
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // Lbl_trabajador
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ID Trabajador";
+            this.Lbl_trabajador.AutoSize = true;
+            this.Lbl_trabajador.Location = new System.Drawing.Point(22, 49);
+            this.Lbl_trabajador.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Lbl_trabajador.Name = "Lbl_trabajador";
+            this.Lbl_trabajador.Size = new System.Drawing.Size(72, 13);
+            this.Lbl_trabajador.TabIndex = 0;
+            this.Lbl_trabajador.Text = "ID Trabajador";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 110);
+            this.label2.Location = new System.Drawing.Point(22, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "ID Obra";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(170, 60);
+            this.textBox1.Location = new System.Drawing.Point(128, 49);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(138, 22);
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(170, 110);
+            this.textBox2.Location = new System.Drawing.Point(128, 89);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(138, 22);
+            this.textBox2.Size = new System.Drawing.Size(104, 20);
             this.textBox2.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 167);
+            this.label3.Location = new System.Drawing.Point(22, 136);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 17);
+            this.label3.Size = new System.Drawing.Size(81, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Dias Laborados";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(170, 162);
+            this.textBox3.Location = new System.Drawing.Point(128, 132);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(138, 22);
+            this.textBox3.Size = new System.Drawing.Size(104, 20);
             this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(103, 230);
+            this.button1.Location = new System.Drawing.Point(77, 187);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 32);
+            this.button1.Size = new System.Drawing.Size(71, 26);
             this.button1.TabIndex = 6;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // controlmano
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 278);
+            this.ClientSize = new System.Drawing.Size(256, 226);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_trabajador);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "controlmano";
             this.Text = "editarobra";
             this.Load += new System.EventHandler(this.controlmano_Load);
@@ -116,7 +126,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lbl_trabajador;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
